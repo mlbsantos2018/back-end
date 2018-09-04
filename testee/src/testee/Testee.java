1e739d910eaf7d7f33a43b1dfc7be24c3c3d5fc3
mlbsantos <mlbsantos@faeterj-10-10.faeterj-petropolis.edu.br>
2018-09-04 09:45:19 -0300

    teste

--- testee/src/testee/Testee.java
+++ testee/src/testee/Testee.java
@@ -15,7 +15,7 @@ public class Testee {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Teste");,
+        System.out.println("Teste");
         
         
         
